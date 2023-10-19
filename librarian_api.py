ff645c0267f21d7a7d092f4a02260095a49e0055
azfarus <samazfar52@gmail.com>
2023-10-19 20:37:10 +0600

    apis

--- librarian_api.py
+++ librarian_api.py
@@ -24,8 +24,22 @@ class librarianApi:
         response= requests.post(url , params=payload , auth=HTTPBasicAuth(username,password))
 
         print(response.status_code)
+    
+    @classmethod
+    def insert_author(self , id ,name , nationality ,username , password):
+        url="http://localhost:8081/librarian/author-entry"
+
+        payload={
+            "id":id,
+            "name":name,
+            "nationality":nationality
+        }
+
+        response= requests.post(url , params=payload , auth=HTTPBasicAuth(username,password))
+
+        print(response.status_code)
 
 
 
 if __name__ == "__main__":
-    librarianApi.insert_book("BAJE BOI" , 12345 ,"2014" , "CSE" , 26 ,15 ,0,0,0,"1234" ,"abcd")
+    librarianApi.insert_book("BAJE BOI" , 12345 ,"2014" , "CSE" , 26 ,15 ,0,0,0,  "1234" ,"abcd")
