170716101e6d42776a6f850b3517a80c221892fd
Radib <radib@iut-dhaka.edu>
2023-10-24 20:26:00 +0600

    auth csv

--- login.py
+++ login.py
@@ -2,12 +2,13 @@ import sys
 import PyQt5
 from PyQt5 import QtGui, QtWidgets , QtCore 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog 
 from PyQt5.uic import loadUi
 from scene2 import Scene2
 import login_api
 from sidebar_ui import Ui_MainWindow
-
+import csv
+from librarian_api import librarianApi
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -15,6 +16,7 @@ class MainWindow(QMainWindow):
         self.ui=loadUi('sidebar.ui', self)
 
         # self.ui.icon_only_widget.hide()
+        self.selected_files = []
         self.ui.full_menu_widget.hide()
         self.ui.stackedWidget.setCurrentIndex(0)
         self.ui.home_btn_2.setChecked(True)
@@ -56,9 +58,16 @@ class MainWindow(QMainWindow):
 
     def on_orders_btn_1_toggled(self):
         self.ui.stackedWidget.setCurrentIndex(2)
+        #self.ui.bookbtn.clicked.connect(self.openFileDialog)
+        self.ui.authbtn.clicked.connect(self.openFileDialog)
+        #self.ui.submitbtn.clicked.connect(self.submitFiles)
+        
 
     def on_orders_btn_2_toggled(self):
         self.ui.stackedWidget.setCurrentIndex(2)
+        #self.ui.bookbtn.clicked.connect(self.openFileDialog)
+        self.ui.authbtn.clicked.connect(self.openFileDialog)
+        #self.ui.submitbtn.clicked.connect(self.submitFiles)
 
     def on_products_btn_1_toggled(self):
         self.ui.stackedWidget.setCurrentIndex(3)
@@ -71,6 +80,43 @@ class MainWindow(QMainWindow):
 
     def on_customers_btn_2_toggled(self):
         self.ui.stackedWidget.setCurrentIndex(4)
+        
+    def openFileDialog(self):
+            options = QFileDialog.Options()
+            # Show the file dialog and get the selected file path
+            filePath, _ = QFileDialog.getOpenFileName(self, "Open File", "", "All Files ();;Text Files (.txt)", options=options)
+            
+            if filePath:
+                print(f'Selected File: {filePath}')   
+                with open(filePath, mode ='r')as file:
+
+                    # reading the CSV file
+                    csvFile = csv.reader(file)
+                    # displaying the contents of the CSV file
+                    for lines in csvFile:
+                            librarianApi.insert_author(lines[0], lines[1], lines[2], entered_username, entered_password)
+    # def openFileDialog(self):
+    #     options = QFileDialog.Options()
+    #     file_paths, _ = QFileDialog.getOpenFileNames(self, "Open File", "", "CSV Files (*.csv);;All Files (*)", options=options)
+
+    #     if file_paths:
+    #         for file_path in file_paths:
+    #             print(f'Selected File: {file_path}')
+    #             # You can store the file paths in a list here
+    #             self.selected_files.append(file_path)
+                
+    
+    # def submitFiles(self):
+    #     # Handle the submission of selected files
+    #     if self.selected_files:
+    #         # Send the selected files to the backend for processing
+    #         for file_path in self.selected_files:
+    #             with open(file_path, mode='r') as file:
+    #                 csvFile = csv.reader(file)
+    #                 for lines in csvFile:
+    #                     print(lines)
+    #                 # Process the CSV file, e.g., send data to the backend
+
 
 
 class MyMainWindow(QtWidgets.QWidget):
@@ -85,7 +131,9 @@ class MyMainWindow(QtWidgets.QWidget):
         self.ui.pushButton.clicked.connect(self.login)
     def login(self):
         # Get the entered username and password
+        global entered_username
         entered_username = self.ui.lineEdit.text()
+        global entered_password 
         entered_password = self.ui.lineEdit_2.text()
         state=login_api.loginController.librarian_login(entered_username, entered_password)
         if(state):
